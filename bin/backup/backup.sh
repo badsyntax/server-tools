9e34c0d531038737948f849fe03820d52cd732de
Richard Willis <willis.rh@gmail.com>
2014-04-10 23:30:01 +0100

    Update backup.sh

--- bin/backup/backup.sh
+++ bin/backup/backup.sh
@@ -1,19 +1,28 @@
 #/usr/bin/env bash
 
-# NOTE: intended to be run as root
+# NOTE: this script should be run by cron as root user
 
 backupdir="/backup/lxc"
-s3bucket="your-bucket-name"
+s3bucket="bucket-name"
 zfspool="lxc"
+ftpuser="username"
+ismounted=$(df -h | grep "$ftpuser")
 
-path="${BASH_SOURCE[0]}"
-cwd=$(dirname "$path")
+# Ensure FTP backup directory is mounted
+if [ -z "$ismounted" ]; then
+        echo "Attempting to mount FTP backup directory"
+        sshfs -o idmap=user "$ftpuser@$ftpuser.your-backup.de:lxc/" "$backupdir"
+        if [ $? -ne 0 ]; then
+                echo "FTP backup directory mount failed!"
+                exit 1
+        fi
+fi
 
 # Backup the zfs containers to the FTP drive
 echo "Backing up ZFS container snapshots at $backupdir to FTP drive"
 for container in $(lxc-ls)
 do
-	"$cwd"/zfs.sh "$backupdir" "$zfspool" "$container"
+	/root/bin/backup/zfs.sh "$backupdir" "$zfspool" "$container"
 	if [ $? -ne 0 ]; then
 		echo "ZFS backup failed for container $container"
 		exit $?
@@ -22,15 +31,18 @@ done
 
 # Backup the containers to S3
 echo "Backing up $backupdir to s3..."
-"$cwd"/s3.sh "$backupdir" "$s3bucket"
+/root/bin/backup/s3.sh "$backupdir" "$s3bucket"
 if [ $? -ne 0 ]; then
         echo "s3 backup failed"
         exit $?
 fi
 
-# Removing backups older than 7 days...
+# Remove backups older than 7 days...
 echo "Cleaning up old backups at $backupdir..."
 find "$backupdir" -mtime +7 -exec rm -f {} \;
 
-echo "Done!"
+# Umount FTP backup drive
+echo "Unmounting FTP backup drive..."
+umount "$backupdir"
 
+echo "Done!"
