01d174d2b02e63c86ae5b846ee65cc4813980a6e
Richard <willis.rh@gmail.com>
2014-04-12 01:45:57 +0200

    More reliable backup script

--- backup/bin/backup.sh
+++ backup/bin/backup.sh
@@ -1,38 +1,40 @@
 #!/usr/bin/env bash
-# Written by Richard Willis <willis.rh@gmail.com>
+#backup.sh, by Richard Willis <willis.rh@gmail.com>
 
-# NOTE: this script should be run by cron as root user
-
-### ADJUST VALUE BELOW ############################################
-
+### ADJUST VALUE BELOW ###
 # Location of config files
 export HOME=/root
+### LEAVE THE REST UNTOUCHED ###
 
-### LEAVE THE REST UNTOUCHED ######################################
+function load_config {
 
-configfile="$HOME/.backupcfg"
+	local configfile="$HOME/.backupcfg"
 
-if [ ! -e "$configfile" ]; then
-	echo "Error: backup config file does not exist at location: $configfile"
-	exit 1
-fi
+	if [ ! -e "$configfile" ]; then
+		echo "Error: backup config file does not exist at location: $configfile"
+		exit 1
+	fi
 
-source "$configfile"
+	source "$configfile"
 
-backupdir="$backup_conf_backupdir"
-s3bucket="$backup_conf_s3bucket"
-zfspool="$backup_conf_zfspool"
-ftpuser="$backup_conf_ftpuser"
-ismounted=$(df -h | grep "$ftpuser")
+	if [ -z "$backupdir" ] || [ -z "$s3bucket" ] || [ -z "$zfspool" ] || [ -z "$ftpuser" ]; then
+		echo "Invalid config!"
+		exit 1
+	fi
+}
 
+# Sync local files with s3
 function backup_s3 {
+	echo -n "Backing up $backupdir to s3..."
 	s3cmd sync --delete-removed "$backupdir" "s3://$s3bucket/"
 	if [ $? -ne 0 ]; then
 		echo "s3cmd sync failed!"
 		exit 1
 	fi
+	echo "done."
 }
 
+# Create an archived zfs snapshot of a lxc container
 function backup_zfs {
 
 	local container="$1"
@@ -40,70 +42,89 @@ function backup_zfs {
 	local snapshot="$zfspool/$container@$timestamp"
 	local backupfile="$backupdir/$container@$timestamp.gz"
 
-	echo "Creating zfs snapshot: $snapshot..."
+	echo -n "Creating zfs snapshot: $snapshot..."
 	zfs snapshot "$snapshot"
 	if [ $? -ne 0 ]; then
 		echo "Unable to create snapshot!"
 		exit 1
 	fi
+	echo "done."
 
-	if [ ! -e "$backupfile" ]; then
-		echo "Creating data backup at location: $backupfile..."
+	if [ -e "$backupfile" ]; then
+		echo "Backup file already exists: $backupfile"
+	else
+		echo -n "Creating data backup at location: $backupfile..."
 		zfs send "$snapshot" | gzip > "$backupfile"
 		if [ $? -ne 0 ]; then
 			echo "Unable to create data backup!"
 			exit 1
 		fi
-	else
-		echo "Backup file already exists: $backupfile"
+		echo "done."
 	fi
 
-	echo "Destroying snapshot: $snapshot..."
+	echo -n "Destroying snapshot: $snapshot..."
 	zfs destroy "$snapshot"
 	if [ $? -ne 0 ]; then
 		echo "Unable to destroy snapshot!"
 		exit 1
 	fi
+	echo "done."
 }
 
-# Ensure FTP backup directory is mounted
-if [ -z "$ismounted" ]; then
-        echo "Attempting to mount FTP backup directory..."
-        sshfs -o idmap=user "$ftpuser@$ftpuser.your-backup.de:lxc/" "$backupdir"
-        if [ $? -ne 0 ]; then
-                echo "FTP backup directory mount failed!"
-                exit 1
-        fi
-fi
+# Remove backups older than 7 days
+function clean_backups {
+	echo -n "Cleaning up old backups at $backupdir..."
+	find "$backupdir" -mtime +7 -exec rm -f {} \;
+	echo "done."
+}
 
-# Backup the zfs containers to the FTP drive
-echo "Backing up ZFS container snapshots at $backupdir to FTP drive"
-for container in $(lxc-ls)
-do
-	backup_zfs "$container"
-	if [ $? -ne 0 ]; then
-		echo "ZFS backup failed for container $container"
-		exit $?
+# Umount the backup drive
+function unmount_backup_drive {
+	echo -n "Unmounting FTP backup drive..."
+	umount "$backupdir"
+	echo "done."
+}
+
+# Backup all containers by taking zfs snapshots
+function backup_containers {
+	echo "Backing up ZFS container snapshots at $backupdir to FTP drive..."
+	for container in $(lxc-ls)
+	do
+		backup_zfs "$container"
+		if [ $? -ne 0 ]; then
+			echo "ZFS backup failed for container $container"
+			exit 1
+		fi
+	done
+}
+
+# Mount the FTP backup drive
+function mount_backup_drive {
+	local ismounted=$(df -h | grep "$ftpuser")
+	if [ -z "$ismounted" ]; then
+		echo -n "Attempting to mount FTP backup directory..."
+		sshfs -o idmap=user "$ftpuser@$ftpuser.your-backup.de:lxc/" "$backupdir"
+		if [ $? -ne 0 ]; then
+			echo "FTP backup directory mount failed!"
+			exit 1
+		fi
+		echo "done."
 	fi
-done
+}
 
-size=$(du -h "$backupdir")
-echo "Total backup size: $size"
+function show_backup_size {
+	size=$(du -h "$backupdir")
+	echo "Total backup size: $size"
+}
 
-# Backup the containers to S3
-echo "Backing up $backupdir to s3..."
-backup_s3 "$backupdir" "$s3bucket"
-if [ $? -ne 0 ]; then
-        echo "s3 backup failed"
-        exit $?
-fi
-
-# Remove backups older than 7 days...
-echo "Cleaning up old backups at $backupdir..."
-find "$backupdir" -mtime +7 -exec rm -f {} \;
-
-# Umount FTP backup drive
-echo "Unmounting FTP backup drive..."
-umount "$backupdir"
-
-echo "Done!"
+function main {
+	load_config
+	mount_backup_drive
+	backup_containers
+	show_backup_size
+	backup_s3
+	clean_backups
+	unmount_backup_drive
+	echo "All tasks completed successfully!"
+}
+main
