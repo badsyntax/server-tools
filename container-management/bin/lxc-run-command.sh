42c4a09cd75a939f7223cd11f61b14b5d44d1887
Richard <willis.rh@gmail.com>
2014-05-28 10:06:29 +0200

    Added getopts

--- container-management/bin/lxc-run-command.sh
+++ container-management/bin/lxc-run-command.sh
@@ -1,32 +1,63 @@
 #/usr/bin/env bash
 
-command="$@"
+if [ "$UID" -ne 0 ]; then
+	echo "Please run as root."
+	exit
+fi
 
-echo -e "Containers:\n"
+usage() {
+	echo "Usage: $0 -n <container> -c <command>"
+	echo "Example: $0 -n richard -c apt-get update"
+	exit 1
+}
 
-for container in $(lxc-ls)
-do
-	if [ "$container" != "ubuntu-base" ] && [ "$container" != "ubuntu-lamp" ]; then
-		echo "$container"
-	fi
+# Parse options
+while getopts ":n:c:" o; do
+	case "${o}" in
+		n)
+			opt_container=${OPTARG}
+			;;
+		c)
+			opt_command=${OPTARG}
+			;;
+		*)
+			usage
+			;;
+	esac
 done
-echo -e "All\n"
+shift $((OPTIND-1))
 
-echo -n "Enter container to run command in: "
-read container
+if [ -z "${opt_container}" ] || [ -z "${opt_command}" ]; then
+	usage
+fi
 
-if [ "$container" = "All" ]; then
-	echo "Running command '$command' in *all* containers..."	
+if [ "$opt_container" != "all" ]; then
+	# Check the container is valid
+	found=0
+	for container in $(lxc-ls)
+	do
+		if [ "$opt_container" = "$container" ]; then
+			found=1
+		fi
+	done
+	if [ $found -eq 0 ]; then
+		echo "Container does not exist!"
+		exit 1
+	fi
+fi
+
+if [ "$opt_container" = "all" ]; then
+	echo "Running command '$opt_command' in *all* containers..."	
 	for container in $(lxc-ls)
 	do
 		if [ "$container" != "ubuntu-base" ] && [ "$container" != "ubuntu-lamp" ]; then
-			echo "Running command '$command' in '$container'..."
-			lxc-attach -n "$container" -- $command	
+			echo "Running command '$opt_command' in '$container'..."
+			lxc-attach -n "$container" -- $opt_command	
 		fi
 	done
 else
-	echo "Running command '$command' in '$container'..."
-	lxc-attach -n "$container" -- $command
+	echo "Running command '$opt_command' in '$opt_container'..."
+	lxc-attach -n "$opt_container" -- $opt_command
 fi
 
-echo "done!"
+echo "Done."
